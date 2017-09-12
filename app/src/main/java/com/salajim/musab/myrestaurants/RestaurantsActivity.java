34be42466df7b5c19807d5bb8b6fc2da131c3778
Musab36 <musabhd@gmail.com>
2017-09-12 14:10:38 +0300

    Fixed some bugs in my Yelp API.

--- app/src/main/java/com/salajim/musab/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/salajim/musab/myrestaurants/RestaurantsActivity.java
@@ -4,23 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class RestaurantsActivity extends AppCompatActivity {
-    @Bind(R.id.locationTextView) TextView mLocationTextView;
-    @Bind(R.id.listView) ListView mListView;
+    public static final String TAG = RestaurantsActivity.class.getSimpleName();
 
-    private String[] restaurants = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food",
-            "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar",
-            "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole"};
+    @Bind(R.id.locationTextView)
+    TextView mLocationTextView;
+    @Bind(R.id.listView)
+    ListView mListView;
+
+    public ArrayList<Restaurant> mRestaurants = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +33,56 @@ public class RestaurantsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
         ButterKnife.bind(this);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
-        mListView.setAdapter(adapter);
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String restaurant = ((TextView)view).getText().toString();
-                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
-                Log.v("RestaurantsActivity", "In the onItemClickListener!");
-            }
-        });
-
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Here are all restaurants near:" + location);
 
-        Log.d("RestaurantsActivity", "In the onCreate method!");
+        mLocationTextView.setText("Here are all the restaurants near: " + location);
 
+        getRestaurants(location);
+    }
+
+    // Callback method for desplaying response data
+    private void getRestaurants(String location) {
+        final YelpService yelpService = new YelpService(); // Creating new instance of Yelp
+        yelpService.findRestaurants(location, new Callback() { // new Callback represents the second argument in findRestaurants() method
+
+            // This method is run when our request fails
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace(); // prints details of the error to the output console
+            }
+
+            // This method is run if our request is successful
+            @Override
+            public void onResponse(Call call, Response response) {
+                mRestaurants = yelpService.processResults(response);// We are triggering .processResults() and collect its return value
+
+                RestaurantsActivity.this.runOnUiThread(new Runnable() { // We are switching to UI thread, Runnable helps sharing code between threads
+                    @Override
+                    public void run() {//Runnable method which contains the code for the UI thread
+                        // String array returning list of restaurants that we want to display for the users
+                        String[] restaurantNames = new String[mRestaurants.size()];
+                        for (int i = 0; i < restaurantNames.length; i++) {
+                            restaurantNames[i] = mRestaurants.get(i).getName();
+                        }
+
+                        // An ArrayAdapter passing data to the view
+                        ArrayAdapter adapter = new ArrayAdapter(RestaurantsActivity.this,
+                                android.R.layout.simple_list_item_1, restaurantNames);
+                        mListView.setAdapter(adapter);
+
+                        for (Restaurant restaurant : mRestaurants) {
+                            Log.d(TAG, "Name: " + restaurant.getName());
+                            Log.d(TAG, "Phone: " + restaurant.getPhone());
+                            Log.d(TAG, "Website: " + restaurant.getWebsite());
+                            Log.d(TAG, "Image url: " + restaurant.getImageUrl());
+                            Log.d(TAG, "Rating: " + Double.toString(restaurant.getRating()));
+                            Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", restaurant.getAddress()));
+                            Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
+                        }
+                    }
+                });
+            }
+        });
     }
 }
