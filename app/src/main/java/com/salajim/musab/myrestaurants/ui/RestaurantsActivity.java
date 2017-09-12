2221a3bfb1941ab3f6ba8a6a41ea5c90bd4c194b
Musab36 <musabhd@gmail.com>
2017-09-12 16:09:45 +0300

    Arranged classes into packages and added RecyclerView.

--- app/src/main/java/com/salajim/musab/myrestaurants/ui/RestaurantsActivity.java
+++ app/src/main/java/com/salajim/musab/myrestaurants/ui/RestaurantsActivity.java
@@ -1,12 +1,15 @@
-package com.salajim.musab.myrestaurants;
+package com.salajim.musab.myrestaurants.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.salajim.musab.myrestaurants.R;
+import com.salajim.musab.myrestaurants.adapter.RestaurantListAdapter;
+import com.salajim.musab.myrestaurants.models.Restaurant;
+import com.salajim.musab.myrestaurants.services.YelpService;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -19,11 +22,14 @@ import okhttp3.Response;
 
 public class RestaurantsActivity extends AppCompatActivity {
     public static final String TAG = RestaurantsActivity.class.getSimpleName();
-
+    /*
     @Bind(R.id.locationTextView)
     TextView mLocationTextView;
     @Bind(R.id.listView)
     ListView mListView;
+    */
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    private RestaurantListAdapter mAdapter;
 
     public ArrayList<Restaurant> mRestaurants = new ArrayList<>();
 
@@ -36,7 +42,7 @@ public class RestaurantsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
 
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
+        //mLocationTextView.setText("Here are all the restaurants near: " + location);
 
         getRestaurants(location);
     }
@@ -60,29 +66,15 @@ public class RestaurantsActivity extends AppCompatActivity {
                 RestaurantsActivity.this.runOnUiThread(new Runnable() { // We are switching to UI thread, Runnable helps sharing code between threads
                     @Override
                     public void run() {//Runnable method which contains the code for the UI thread
-                        // String array returning list of restaurants that we want to display for the users
-                        String[] restaurantNames = new String[mRestaurants.size()];
-                        for (int i = 0; i < restaurantNames.length; i++) {
-                            restaurantNames[i] = mRestaurants.get(i).getName();
-                        }
+                        mAdapter = new RestaurantListAdapter(getApplicationContext(), mRestaurants);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(RestaurantsActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
 
-                        // An ArrayAdapter passing data to the view
-                        ArrayAdapter adapter = new ArrayAdapter(RestaurantsActivity.this,
-                                android.R.layout.simple_list_item_1, restaurantNames);
-                        mListView.setAdapter(adapter);
-
-                        for (Restaurant restaurant : mRestaurants) {
-                            Log.d(TAG, "Name: " + restaurant.getName());
-                            Log.d(TAG, "Phone: " + restaurant.getPhone());
-                            Log.d(TAG, "Website: " + restaurant.getWebsite());
-                            Log.d(TAG, "Image url: " + restaurant.getImageUrl());
-                            Log.d(TAG, "Rating: " + Double.toString(restaurant.getRating()));
-                            Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", restaurant.getAddress()));
-                            Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
                         }
-                    }
-                });
-            }
-        });
+                    });
+                }
+            });
     }
 }
